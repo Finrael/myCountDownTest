10a9933875dc32688836fc189df5950c290b60a8
Ovidio Martinez <finrosdev@gmail.com>
2019-10-10 13:26:48 -0700

    a bit of css for clarity

--- src/components/countDownWithLibrary.jsx
+++ src/components/countDownWithLibrary.jsx
@@ -2,43 +2,48 @@ import React from 'react';
 import './countdownFormat.css';
 import Countdown from 'react-countdown-now';
 
-class CountdownWithLibrary extends React.Component{
-    constructor(props){
+class CountdownWithLibrary extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            timeToCountDown:3,
-            format:'',
-            numberCountdown:0,
-            displayType:0,
-            formatedTime:'',
+        this.state = {
+            timeToCountDown: 3,
+            format: '',
+            numberCountdown: 0,
+            displayType: 0,
+            formatedTime: '',
         }
     }
-    assignTime=(e)=>{
-        this.setState({timeToCountDown:e.target.value});
+    assignTime = (e) => {
+        this.setState({ timeToCountDown: e.target.value });
     }
-    assignDisplay=(e)=>{
-        this.setState({displayType:e.target.value});
+    assignDisplay = (e) => {
+        this.setState({ displayType: e.target.value });
     }
-    count = ()=>{
+    count = () => {
         let count = this.state.timeToCountDown;
-        while (count>0){
+        while (count > 0) {
             console.log(count, 'format type', this.state.displayType);
             count--;
         }
     }
 
-    render(){
-        return(
-          <div>
-              <div className='inputTime'>
-               <label>On format select 0 for miliseconds and anything else for seconds</label>
-                  <input type='text' placeholder='input time to countdown in seconds' onChange={this.assignTime}/>
-                  <input type='text' placeholder='input desired format' onChange={this.assignDisplay}/>
-              </div>   
-            {
-            this.state.displayType===0 ?<Countdown date={Date.now() + (this.state.timeToCountDown * 1000)} precision={1} intervalDelay={0} renderer={props => <div>{props.total}</div>} /> :  <Countdown date={Date.now() + (this.state.timeToCountDown * 1000)}/>
-          }
-          </div>  
+    render() {
+        return (
+            <div>
+
+                <label className='options'>On format select 0 for miliseconds and anything else for seconds</label>
+                <div className='inputTime'>
+                    <div>
+                    <input type='text' placeholder='input time to countdown in seconds' onChange={this.assignTime}/>
+                    </div>
+                    <input type='text' placeholder='input desired format' onChange={this.assignDisplay} />
+                </div>
+                <div className='holder'>
+                    {
+                        this.state.displayType === 0 ? <Countdown date={Date.now() + (this.state.timeToCountDown * 1000)} precision={1} intervalDelay={0} renderer={props => <div>{props.total}</div>} /> : <Countdown date={Date.now() + (this.state.timeToCountDown * 1000)} />
+                    }
+                </div>
+            </div>
         )
     }
 }
